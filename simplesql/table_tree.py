feb35a1f5ebe602163ffc11a96dd3ae123245b7c
Peter Woyzbun <peter.woyzbun@gmail.com>
2017-10-15 19:10:40 -0700

    TableTree base logic GTG.
    Signed-off-by: Peter Woyzbun <peter.woyzbun@gmail.com>

--- simplesql/table_tree.py
+++ simplesql/table_tree.py
@@ -9,6 +9,7 @@ class TableTree(object):
         self._graph = nx.DiGraph()
         self.root_tbl_name = None
         self.sub_query_count = itertools.count()
+        self.table_query_indices = dict()
 
     @property
     def sub_query_index(self):
@@ -16,7 +17,7 @@ class TableTree(object):
 
     def table_parent(self, tbl_name):
         if nx.ancestors(self._graph, tbl_name):
-            return nx.ancestors(self._graph, tbl_name)[0]
+            return list(nx.ancestors(self._graph, tbl_name))[0]
         else:
             return None
 
@@ -44,9 +45,11 @@ class TableTree(object):
         if not self.root_tbl_name:
             self.root_tbl_name = tbl_name
         if not self._graph.has_node(tbl_name):
+            sub_query_index = self.sub_query_index
             self._graph.add_node(tbl_name)
             self._graph.node[tbl_name]['columns'] = list()
-            self._graph.node[tbl_name]['sub_query_index'] = self.sub_query_index
+            self._graph.node[tbl_name]['sub_query_index'] = sub_query_index
+            self.table_query_indices[tbl_name] = sub_query_index
         if parent_tbl_name:
             self._graph.add_edge(parent_tbl_name, tbl_name)
 
@@ -62,4 +65,8 @@ table_tree.add_node_column(tbl_name='part', col_name='date')
 table_tree.add_node_column(tbl_name='supplier', col_name='part_number')
 table_tree.add_node(tbl_name='location', parent_tbl_name='supplier')
 table_tree.add_node_column(tbl_name='location', col_name='name')
-print(table_tree.ordered_select_data())
+for tbl_name in table_tree.ordered_table_names:
+    print(tbl_name)
+    print(table_tree.table_query_indices[tbl_name])
+    print(table_tree.table_parent(tbl_name))
+    print(table_tree.parent_sub_query_index(for_table=tbl_name))
